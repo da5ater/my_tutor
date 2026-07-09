ba33a6898da264e21f71913f7e038d068e73dcaa
MohamedKH <vincentvan205ater@gmail.com>
2026-07-09 08:25:14 +0300

    chore: update action_mailer configuration and settings in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,29 +78,31 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+# Enable DNS rebinding protection and other `Host` header attacks.
+# config.hosts = [
+#   "example.com",     # Allow requests from example.com
+#   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+# ]
+#
+# Skip DNS rebinding protection for the default health check endpoint.
+# config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: ENV["APP_DOMAIN"] || "my-tutor-production.up.railway.app", protocol: "https" }
+config.action_mailer.default_url_options = {
+  host: ENV["APP_DOMAIN"] || "my-tutor-production.up.railway.app",
+  protocol: "https"
+}
 
-# SMTP configuration reading from environment variables
 config.action_mailer.smtp_settings = {
-  address:              ENV["SMTP_ADDRESS"] || "smtp-relay.brevo.com",
-  port:                 (ENV["SMTP_PORT"] || 587).to_i,
-  domain:               ENV["SMTP_DOMAIN"] || "railway.app",
-  user_name:            ENV["SMTP_USERNAME"],
-  password:             ENV["SMTP_PASSWORD"],
-  authentication:       "plain",
+  address: ENV["SMTP_ADDRESS"] || "smtp-relay.brevo.com",
+  port: (ENV["SMTP_PORT"] || 587).to_i,
+  domain: ENV["SMTP_DOMAIN"] || "railway.app",
+  user_name: ENV["SMTP_USERNAME"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: :plain,
   enable_starttls_auto: true
 }
 end
